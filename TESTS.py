bfbc09e31adb1ef1adf2431a906a1ef6a6143efa
aisyahsofia <124658840+aisyahsofia@users.noreply.github.com>
2024-11-15 23:57:50 +0800

    Update TESTS.py

--- TESTS.py
+++ TESTS.py
@@ -32,25 +32,25 @@ class VideoTransformer(VideoTransformerBase):
     def transform(self, frame):
         img = frame.to_ndarray(format="bgr24")
 
-        # Image gray
+        # Directly process the whole image (no face detection)
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(image=img_gray, scaleFactor=1.3, minNeighbors=5)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img=img, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=2)
-            roi_gray = img_gray[y:y + h, x:x + w]
-            roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
-            if np.sum([roi_gray]) != 0:
-                roi = roi_gray.astype('float') / 255.0
-                roi = img_to_array(roi)
-                roi = np.expand_dims(roi, axis=0)
-                prediction = classifier.predict(roi)[0]
-                maxindex = int(np.argmax(prediction))
-                finalout = emotion_dict[maxindex]
-                output = str(finalout)
-            label_position = (x, y)
-            cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        roi_gray = cv2.resize(img_gray, (48, 48), interpolation=cv2.INTER_AREA)
+        
+        if np.sum([roi_gray]) != 0:
+            roi = roi_gray.astype('float') / 255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi, axis=0)
+            prediction = classifier.predict(roi)[0]
+            maxindex = int(np.argmax(prediction))
+            finalout = emotion_dict[maxindex]
+            output = str(finalout)
+        
+        # Display the prediction label on the frame
+        label_position = (50, 50)  # Just position it at a fixed location for now
+        cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
         return img
+
 
 def main():
     # ASL detection application #
